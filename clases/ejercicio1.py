ec39c79e7fc18090377fc484bf4b5a45f29f7522
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-23 16:40:37 +0200

    función limpiar

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -3,3 +3,11 @@ matriz = [[1,1,1,3],
           [2, 2, 2, 7], 
           [3, 3, 3, 9], 
           [4, 4, 4, 13]]
+
+def limpiar():
+    for i in matriz:
+        for j in matriz:
+            if sum(matriz[i][:-1]) == matriz[i][-1]:
+                pass
+            else:
+                matriz[i][-1] = sum(matriz[i][:-1])
