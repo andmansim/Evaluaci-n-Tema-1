ac9294b4fc55ec7b883bb50735f26522d58c8081
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-23 17:11:54 +0200

    terminado ejercicio 3

--- clases/ejercicio3.py
+++ clases/ejercicio3.py
@@ -1,5 +1,7 @@
-lista = []
+
 def generar_listas(inicio, fin, salto):
-    for i in range(inicio, fin, salto):
+    lista = []
+    for i in range(inicio, fin + 1, salto):
         lista.append(i)
+    return lista
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from clases.ejercicio2 import *
 from clases.ejercicio1 import*
+from clases.ejercicio3 import*
 
 if __name__=='__main__':
     print('Qué ejercicio quieres ejecutar?')
@@ -13,11 +14,23 @@ if __name__=='__main__':
         prueba = c.comprobar()
         print(prueba)
     elif u == 1:
-    #Ejercicio 1
+        #Ejercicio 1
         matriz = [[1,1,1,3],
           [2, 2, 2, 7], 
           [3, 3, 3, 9], 
           [4, 4, 4, 13]]
         limpiar(matriz)
         print(matriz)
-    
+    elif u == 3:
+
+        lista1 = generar_listas(0, 10, 1)
+        print(lista1)
+        lista2 = generar_listas(-10, 0, 1)
+        print(lista2)
+        lista3 = generar_listas(0, 20, 2)
+        print(lista3)
+        lista4 = generar_listas(-20, 0, 2)
+        print(lista4)
+        lista5 = generar_listas(0, 50, 5)
+        print(lista5)
+        
