c39e4cadcc8f4737468cdd23a08da355ab4ef640
andmansim <91722324+andmansim@users.noreply.github.com>
2022-09-23 16:34:34 +0200

    organización ejercicios

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from clases.ejercicio2 import *
 
 if __name__=='__main__':
+    #Ejercicio 2
     c = Cadena()
     print('Introduzca una frase')
     usuario = str(input())
     c.usuario(usuario)
-    print(c.usuario)
     prueba = c.comprobar()
     print(prueba)
+    
+    #Ejercicio 1
+    
